9d41881bd265b4399880433b095f24b42050e83b
シロボタン <shirobutton@gmail.com>
2022-03-04 18:43:23 +0900

    first

---
+++ app/src/main/java/com/shirobutton/dependency_inversion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shirobutton.dependency_inversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
