742fffd02671f1523369b7638890a4aa266a8e75
シロボタン <shirobutton@gmail.com>
2022-03-05 10:33:40 +0900

    Add button and number to MainActivity

--- app/src/main/java/com/shirobutton/dependency_inversion/MainActivity.kt
+++ app/src/main/java/com/shirobutton/dependency_inversion/MainActivity.kt
@@ -2,10 +2,21 @@ package com.shirobutton.dependency_inversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shirobutton.dependency_inversion.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var number: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.textView.text = number.toString()
+        binding.button.setOnClickListener {
+            // TODO
+        }
+        setContentView(binding.root)
     }
 }
