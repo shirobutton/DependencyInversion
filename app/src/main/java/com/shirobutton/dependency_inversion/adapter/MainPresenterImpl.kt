335c51218a0b5e0cdc6344d91062aa20628f7450
シロボタン <shirobutton@gmail.com>
2022-03-06 16:29:11 +0900

    Change MainPresenterImpl to use MainReceiver

--- app/src/main/java/com/shirobutton/dependency_inversion/adapter/MainPresenterImpl.kt
+++ app/src/main/java/com/shirobutton/dependency_inversion/adapter/MainPresenterImpl.kt
@@ -1,12 +1,11 @@
 package com.shirobutton.dependency_inversion.adapter
 
-import com.shirobutton.dependency_inversion.driver.MainActivity
 import javax.inject.Inject
 
 class MainPresenterImpl @Inject constructor(
-    private val mainActivity: MainActivity
+    private val mainReceiver: MainReceiver
 ): MainPresenter {
     override fun setNumber(number: Int) {
-        mainActivity.onReceiveNumber(number)
+        mainReceiver.onReceiveNumber(number)
     }
 }
