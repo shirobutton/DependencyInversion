509af4148086acb54c521c17578ad24ac4149cdd
シロボタン <shirobutton@gmail.com>
2022-03-06 17:00:43 +0900

    Change AdapterModule to binds IncrementUseCaseOutputBoundary

--- app/src/main/java/com/shirobutton/dependency_inversion/di/AdapterModule.kt
+++ app/src/main/java/com/shirobutton/dependency_inversion/di/AdapterModule.kt
@@ -4,8 +4,10 @@ import com.shirobutton.dependency_inversion.adapter.MainController
 import com.shirobutton.dependency_inversion.adapter.MainControllerImpl
 import com.shirobutton.dependency_inversion.adapter.MainPresenter
 import com.shirobutton.dependency_inversion.adapter.MainPresenterImpl
+import com.shirobutton.dependency_inversion.application.IncrementUseCaseOutputBoundary
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
@@ -20,4 +22,10 @@ abstract class AdapterModule {
     @ActivityScoped
     @Binds
     abstract fun bindMainPresenter(mainPresenterImpl: MainPresenterImpl): MainPresenter
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindIncrementUseCaseOutputBoundary(
+        mainPresenter: MainPresenter
+    ): IncrementUseCaseOutputBoundary
 }
