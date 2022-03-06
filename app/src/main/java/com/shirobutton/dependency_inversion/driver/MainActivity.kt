5dff41978c795a5c5980c5584cde6b6c25e5c000
シロボタン <shirobutton@gmail.com>
2022-03-06 13:16:15 +0900

    Change to inject controller to MainActivity

--- app/src/main/java/com/shirobutton/dependency_inversion/driver/MainActivity.kt
+++ app/src/main/java/com/shirobutton/dependency_inversion/driver/MainActivity.kt
@@ -2,11 +2,16 @@ package com.shirobutton.dependency_inversion.driver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shirobutton.dependency_inversion.adapter.MainController
 import com.shirobutton.dependency_inversion.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var controller: MainController
 
     private var number: Int = 0
 
@@ -15,7 +20,8 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         binding.textView.text = number.toString()
         binding.button.setOnClickListener {
-            // TODO
+            number = controller.onIncrementButtonClick(number)
+            binding.textView.text = number.toString()
         }
         setContentView(binding.root)
     }
